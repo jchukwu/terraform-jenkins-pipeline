de009b6e6ff067a41b131f9f1fae95fcd43d62e5
jchukwu <84819504+jchukwu@users.noreply.github.com>
2021-07-22 22:06:56 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "code-sharing-bucket112"
     key = "jt.tfstate"
     region = "us-west-2"
     dynamodb_table = "tfstate"
